741a703f587de650ddb5bedd014e133c6efbd305
Nikolai <nikom87@gmail.com>
2013-05-08 18:14:51 +0600

    Перенос однотипных переменных в массив

--- DiceGame/Class1.cs
+++ DiceGame/Class1.cs
@@ -11,120 +11,94 @@ namespace DiceGame
         {
             int scope = 0;
 
-            int count1 = 0;
-            int count2 = 0;
-            int count3 = 0;
-            int count4 = 0;
-            int count5 = 0;
-            int count6 = 0;
-
+            int[] count = new int[7];
+            
             foreach ( int a in x )
             {
-                switch ( a )
-                {
-                    case 1:
-                        count1++;
-                        break;
-                    case 2:
-                        count2++;
-                        break;
-                    case 3:
-                        count3++;
-                        break;
-                    case 4:
-                        count4++;
-                        break;
-                    case 5:
-                        count5++;
-                        break;
-                    case 6:
-                        count6++;
-                        break;
-                }
+                count[a]++;
             }
 
-            if ( count1 >= 1 && count2 >= 1 && count3 >= 1 && count4 >= 1 && count5 >= 1 )
+            if ( count[1] >= 1 && count[2] >= 1 && count[3] >= 1 && count[4] >= 1 && count[5] >= 1 )
             {
                 scope += 8000;
-                count1 -= 1;
-                count2 -= 1;
-                count3 -= 1;
-                count4 -= 1;
-                count5 -= 1;
+                count[1] -= 1;
+                count[2] -= 1;
+                count[3] -= 1;
+                count[4] -= 1;
+                count[5] -= 1;
             }
 
-            if ( count2 >= 1 && count3 >= 1 && count4 >= 1 && count5 >= 1 && count6 >= 1 )
+            if ( count[2] >= 1 && count[3] >= 1 && count[4] >= 1 && count[5] >= 1 && count[6] >= 1 )
             {
                 scope += 8000;
-                count2 -= 1;
-                count3 -= 1;
-                count4 -= 1;
-                count5 -= 1;
-                count6 -= 1;
+                count[2] -= 1;
+                count[3] -= 1;
+                count[4] -= 1;
+                count[5] -= 1;
+                count[6] -= 1;
             }
 
-            if ( count1 >= 5 )
+            if ( count[1] >= 5 )
             {
                 scope += 4000;
-                count1 -= 5;
+                count[1] -= 5;
             }
             
-            if ( count1 >= 4 )
+            if ( count[1] >= 4 )
             {
                 scope += 2000;
-                count1 -= 4;
+                count[1] -= 4;
             }
 
-            if ( count1 >= 3 )
+            if ( count[1] >= 3 )
             {
                 scope += 1000;
-                count1 -= 3;
+                count[1 ]-= 3;
             }
 
-            if ( count2 >= 3 )
+            if ( count[2] >= 3 )
             {
                 scope += 200;
-                count2 -= 3;
+                count[2] -= 3;
             }
 
-            if ( count3 >= 3 )
+            if ( count[3] >= 3 )
             {
                 scope += 300;
-                count3 -= 3;
+                count[3] -= 3;
             }
 
-            if ( count4 >= 3 )
+            if ( count[4] >= 3 )
             {
                 scope += 400;
-                count4 -= 3;
+                count[4] -= 3;
             }
 
-            if ( count5 >= 3 )
+            if ( count[5] >= 3 )
             {
                 scope += 500;
-                count5 -= 3;
+                count[5] -= 3;
             }
 
-            if ( count6 >= 3 )
+            if ( count[6 ]>= 3 )
             {
                 scope += 600;
-                count6 -= 3;
+                count[6] -= 3;
             }
 
-            if ( count1 >= 1 )
+            if ( count[1] >= 1 )
             {
                 scope += 100;
-                count1 -= 1;
+                count[1] -= 1;
             }
 
-            if ( count5 >= 1 )
+            if ( count[5] >= 1 )
             {
                 scope += 50;
-                count5 -= 1;
+                count[5] -= 1;
             }
 
             return scope;
         }
     }
 }
-
