dfbf72aa7341b01340be17b6f86216da4951167d
Nikolai <nikom87@gmail.com>
2013-05-13 12:46:42 +0600

    Переименование файлов

--- DiceGame.Tests/CalculatorTest.cs
+++ DiceGame.Tests/CalculatorTest.cs
@@ -4,70 +4,77 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DiceGame.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class CalculatorTest
     {
+        Calculator calculator;
+
+        public CalculatorTest()
+        {
+            calculator = new Calculator();
+        }
+
         [TestMethod]
         public void TestOneCombinations()
         {
-            int scope = TCalculator.Run( new int[] { 1, 0, 0, 0, 0 } );
+            int scope = calculator.Run( new int[] { 1, 0, 0, 0, 0 } );
             Assert.AreEqual( 100, scope );
 
-            scope = TCalculator.Run( new int[] { 0, 5, 0, 0, 0 } );
+            scope = calculator.Run( new int[] { 0, 5, 0, 0, 0 } );
             Assert.AreEqual( 50, scope );
         }
 
         [TestMethod]
         public void TestTheeCombinations()
         {
-            int scope = TCalculator.Run( new int[] { 1, 1, 1, 0, 0 } );
+            int scope = calculator.Run( new int[] { 1, 1, 1, 0, 0 } );
             Assert.AreEqual( 1000, scope );
 
-            scope = TCalculator.Run( new int[] { 2, 2, 2, 0, 0 } );
+            scope = calculator.Run( new int[] { 2, 2, 2, 0, 0 } );
             Assert.AreEqual( 200, scope );
 
-            scope = TCalculator.Run( new int[] { 3, 3, 3, 0, 0 } );
+            scope = calculator.Run( new int[] { 3, 3, 3, 0, 0 } );
             Assert.AreEqual( 300, scope );
 
-            scope = TCalculator.Run( new int[] { 4, 4, 4, 0, 0 } );
+            scope = calculator.Run( new int[] { 4, 4, 4, 0, 0 } );
             Assert.AreEqual( 400, scope );
 
-            scope = TCalculator.Run( new int[] { 5, 5, 5, 0, 0 } );
+            scope = calculator.Run( new int[] { 5, 5, 5, 0, 0 } );
             Assert.AreEqual( 500, scope );
 
-            scope = TCalculator.Run( new int[] { 6, 6, 6, 0, 0 } );
+            scope = calculator.Run( new int[] { 6, 6, 6, 0, 0 } );
             Assert.AreEqual( 600, scope );
         }
 
         [TestMethod]
         public void TestMixCombinations()
         {
-            int scope = TCalculator.Run( new int[] { 1, 1, 1, 5, 1 } );
+            int scope = calculator.Run( new int[] { 1, 1, 1, 5, 1 } );
             Assert.AreEqual( 2050, scope );
 
-            scope = TCalculator.Run( new int[] { 2, 3, 4, 6, 2 } );
+            scope = calculator.Run( new int[] { 2, 3, 4, 6, 2 } );
             Assert.AreEqual( 0, scope );
 
-            scope = TCalculator.Run( new int[] { 3, 4, 5, 3, 3 } );
+            scope = calculator.Run( new int[] { 3, 4, 5, 3, 3 } );
             Assert.AreEqual( 350, scope );
         }
 
         [TestMethod]
         public void TestFourCombinations()
         {
-            int scope = TCalculator.Run( new int[] { 1, 1, 1, 1, 0 } );
+            int scope = calculator.Run( new int[] { 1, 1, 1, 1, 0 } );
             Assert.AreEqual( 2000, scope );
         }
 
         [TestMethod]
         public void TestFiveCombinations()
         {
-            int scope = TCalculator.Run( new int[] { 1, 1, 1, 1, 1 } );
+            int scope = calculator.Run( new int[] { 1, 1, 1, 1, 1 } );
             Assert.AreEqual( 4000, scope );
 
-            scope = TCalculator.Run( new int[] { 1, 2, 3, 4, 5 } );
+            scope = calculator.Run( new int[] { 1, 2, 3, 4, 5 } );
             Assert.AreEqual( 8000, scope );
 
-            scope = TCalculator.Run( new int[] { 2, 3, 4, 5, 6 } );
+            scope = calculator.Run( new int[] { 2, 3, 4, 5, 6 } );
             Assert.AreEqual( 8000, scope );
         }
     }
---
+++ DiceGame/Calculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiceGame
+{
+    public class Calculator
+    {
+        private int[] count;
+
+        public int Run( int[] x )
+        {
+            Init( x );
+            return CalcScope();
+        }
+
+        private void Init( int[] x )
+        {
+            count = new int[7];
+            foreach ( int a in x )
+            {
+                count[a]++;
+            }
+        }
+
+        private int CalcScope()
+        {
+            int scope = 0;
+
+            scope += GetScope( new int[] { 1, 1, 1, 1, 1, 0 }, 8000 );
+            scope += GetScope( new int[] { 0, 1, 1, 1, 1, 1 }, 8000 );
+            scope += GetScope( new int[] { 5, 0, 0, 0, 0, 0 }, 4000 );
+            scope += GetScope( new int[] { 4, 0, 0, 0, 0, 0 }, 2000 );
+            scope += GetScope( new int[] { 3, 0, 0, 0, 0, 0 }, 1000 );
+            scope += GetScope( new int[] { 0, 0, 0, 0, 0, 3 }, 600 );
+            scope += GetScope( new int[] { 0, 0, 0, 0, 3, 0 }, 500 );
+            scope += GetScope( new int[] { 0, 0, 0, 3, 0, 0 }, 400 );
+            scope += GetScope( new int[] { 0, 0, 3, 0, 0, 0 }, 300 );
+            scope += GetScope( new int[] { 0, 3, 0, 0, 0, 0 }, 200 );
+            scope += GetScope( new int[] { 1, 0, 0, 0, 0, 0 }, 100 );
+            scope += GetScope( new int[] { 0, 0, 0, 0, 1, 0 }, 50 );
+
+            return scope;
+        }
+
+        private bool CheckSet( int[] set )
+        {
+            bool res = true;
+            for ( int i = 0; i < set.Length; i++ )
+            {
+                res &= count[i + 1] >= set[i];
+            }
+
+            return res;
+        }
+
+        private void RemoveSet( int[] set )
+        {
+            for ( int i = 0; i < set.Length; i++ )
+            {
+                count[i + 1] -= set[i];
+            }
+        }
+
+        private int GetScope( int[] set, int scope )
+        {
+            if ( CheckSet( set ) )
+            {
+                RemoveSet( set );
+                return scope;
+            }
+            return 0;
+        }
+    }
+}
